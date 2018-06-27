a534e2fa5233fb7873cf483b291c80fb957776db
zengwei <18279047775@163.com>
2018-06-27 17:25:35 +0800

    增加mysql查询 占位符方式

--- server/mysql/index.js
+++ server/mysql/index.js
@@ -5,13 +5,26 @@ let connection = mysql.createConnection(root());
 
 connection.connect();
 
+// 重写query方法 解决回调result参数RowDataPacket问题
 let query = connection.query.bind(connection);
-connection.query = (sql,callback) => {
-  query(sql,(err,result,fields) => {
-    let str = JSON.stringify(result);
-    let data = JSON.parse(str);
-    callback(err,data,fields);
-  });
+connection.query = (sql,values,callback) => {
+  // 当前为 占位符方式
+  if(callback) {
+    query(sql,values,(err,result,fields) => {
+      console.log(result);
+      let str = JSON.stringify(result);
+      let data = JSON.parse(str);
+      callback(err,data,fields);
+    });
+  }else{
+    query(sql,(err,result,fields) => {
+      let str = JSON.stringify(result);
+      let data = JSON.parse(str);
+      values(err,data,fields);
+    });
+  }
+
+
 };
 
 export default connection
