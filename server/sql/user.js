dabc6e698eb8e1a10d1e36a22c0c6588fd57189d
zengwei <18279047775@163.com>
2018-07-05 16:41:08 +0800

    修改 mysql 语法 ，

--- server/sql/user.js
+++ server/sql/user.js
@@ -8,6 +8,7 @@ let sqlMap = {
 // 新增 INSERT INTO `user` (userName,passWord)  VALUES ('张三','张三密码');
 // 修改 UPDATE `user` SET userName = '张三-修改' , passWord = '新密码-修改' WHERE userName = '张三'
 // 删除 DELETE from `user` WHERE userName = '张三-修改'
+import moment from 'moment'
 
 let connection = import_module('/mysql/index');
 let sqlMap = {
@@ -40,7 +41,11 @@ let sqlMap = {
   // 用户新增
   userInset({uuid,userName,passWord,nickName}) {
     return new Promise((resolve,reject) => {
-      connection.querypoll(`INSERT INTO user (id,userName,passWord,nickName)  VALUES ("${uuid}","${userName}","${passWord}","${nickName}");`,(err,result) => {
+      // 获取当前用户创建时间
+      let createTime = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
+
+      connection.querypoll(`INSERT INTO user (id,userName,passWord,nickName,createTime)  VALUES (?,?,?,?,?);`,
+      [uuid,userName,passWord,nickName,createTime],(err,result) => {
         resolve({err,result});
       });
     });
@@ -48,7 +53,9 @@ let sqlMap = {
   // 用户修改
   userUpdata({id,userName,passWord,nickName}) {
     return new Promise((resolve,reject) => {
-      connection.querypoll(`UPDATE user SET userName="${userName}",passWord="${passWord}",nickName="${nickName}" WHERE id="${id}";`,(err,result) => {
+      connection.querypoll(`UPDATE user SET userName=?,passWord=?,nickName=? WHERE id=?;`,
+      [userName,passWord,nickName,id]
+      ,(err,result) => {
         resolve({err,result});
       });
     });
@@ -60,7 +67,7 @@ let sqlMap = {
         resolve({err : '无法删除'});
         return;
       }
-      connection.querypoll(`DELETE from user WHERE id='${id}';`,(err,result) => {
+      connection.querypoll(`DELETE from user WHERE id=?;`,[id],(err,result) => {
         resolve({err,result});
       });
     });
@@ -68,7 +75,9 @@ let sqlMap = {
   // 用户列表
   queryUserList({userName,nickName}) {
     return new Promise((resolve,reject) => {
-      connection.querypoll(`SELECT * FROM user WHERE userName like "%${userName}%" and nickName like "%${nickName}%";`,(err,result) => {
+      connection.querypoll(`SELECT * FROM user WHERE userName like ? and nickName like ?;`,
+      [`%${userName}%`,`%${nickName}%`]
+      ,(err,result) => {
         resolve({err,result});
       });
     });
@@ -76,7 +85,7 @@ let sqlMap = {
   // 用户详情
   queryUserDetail({id}) {
     return new Promise((resolve,reject) => {
-      connection.querypoll(`SELECT * FROM user WHERE id="${id}";`,(err,result) => {
+      connection.querypoll(`SELECT * FROM user WHERE id=?;`,[id],(err,result) => {
         resolve({err,result});
       });
     });
@@ -84,7 +93,7 @@ let sqlMap = {
   // 根据用户 id 获取用户img
   queryUserImgs({id}) {
     return new Promise((resolve,reject) => {
-      connection.querypoll(`SELECT * FROM user WHERE id="${id}";`,(err,result) => {
+      connection.querypoll(`SELECT * FROM user WHERE id=?;`,[id],(err,result) => {
         resolve({err,result});
       });
     });
@@ -92,10 +101,10 @@ let sqlMap = {
   // 修改用户图片
   updataUserImgs({id,img01,img02}) {
     return new Promise((resolve,reject) => {
-      let i01 = `UPDATE user SET img01=? WHERE id="${id}";`
-      let i02 = `UPDATE user SET img02=? WHERE id="${id}";`
+      let i01 = `UPDATE user SET img01=? WHERE id=?;`
+      let i02 = `UPDATE user SET img02=? WHERE id=?;`
       let type = img01 !== undefined ? 'img01' : 'img02';
-      connection.querypoll(type === 'img01' ? i01 : i02,[type === 'img01' ? img01 : img02],(err,result) => {
+      connection.querypoll(type === 'img01' ? i01 : i02,[type === 'img01' ? img01 : img02,id],(err,result) => {
         resolve({err,result});
       });
     });
