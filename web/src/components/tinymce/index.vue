b088877c107c8f9db88ad408ee2bbc55e316119c
zengwei <18279047775@163.com>
2018-07-27 18:04:55 +0800

    富文本 增加中文

--- web/src/components/tinymce/index.vue
+++ web/src/components/tinymce/index.vue
@@ -12,8 +12,11 @@
   // 引入样式
   import 'tinymce/skins/lightgray/skin.min.css'
   import 'tinymce/skins/lightgray/content.min.css'
+  // 引入语言包
+  import './langs/zh_CN.js'
 
   export default {
+    
     data() {
       return {
         id : `tinymce-${+ new Date()}`,
@@ -21,6 +24,7 @@
     },
     mounted() {
       tinymce.init({
+        language : 'zh_CN',
         selector : `#${this.id}`,
       });
     },
