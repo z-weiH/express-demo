4839d99deacb43285710f5e46e56e1df668d15f9
zengwei <18279047775@163.com>
2018-09-29 10:06:46 +0800

    完善 pdfjs 页面

--- web/src/views/pdfjs/index.vue
+++ web/src/views/pdfjs/index.vue
@@ -1,15 +1,10 @@
 <template>
   <div class="pdfjs-box">
-    <iframe src="/static/generic/web/viewer.html?file=/demo.pdf"></iframe>
+    <iframe src="/static/generic/web/viewer.html?file=http://image.cache.timepack.cn/nodejs.pdf"></iframe>
   </div>
 </template>
 
 <script>
-  /* 
-    pdf.js 通过github 生成代码后 ， 生成 generic/web/viewer.html 文件
-    通过参数file传递服务器本地静态资源路径， 生成pdf
-  
-  */
   export default {
     
   }
