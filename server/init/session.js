33dd4c4a3524223a40bf7f0d00cb92e5c364317c
zengwei <18279047775@163.com>
2019-05-14 16:49:30 +0800

    处理 CSRF 攻击

--- server/init/session.js
+++ server/init/session.js
@@ -12,6 +12,8 @@ export default (app) => {
     saveUninitialized: false, // 是否保存未初始化的会话
     cookie : {
       maxAge : user_session, // 设置 session 的有效时间，单位毫秒
+      // 处理 CSRF 攻击
+      sameSite : 'strict',
     },
     store : sessionStore, // session 存储到mysql 如果不使用会存储到内存中，node重启 ， 会重置session
   }));
